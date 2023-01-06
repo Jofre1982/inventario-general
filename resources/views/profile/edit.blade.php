0b446dfb6e97761ba331e76af4ce9df5ca4f7e23
Jofre <jofre1706@gmail.com>
2023-01-06 02:42:16 -0500

    revision template y seeder

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app', ['page' => __('Perfil del Usuario'), 'pageSlug' => 'profile'])
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="title">{{ __('Editar Usuario') }}</h5>
+                </div>
+                <form method="post" action="{{ route('profile.update') }}" autocomplete="off">
+                    <div class="card-body">
+                            @csrf
+                            @method('put')
+
+                            @include('alerts.success')
+
+                            <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                                <label>{{ __('Nombre') }}</label>
+                                <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{ old('name', auth()->user()->name) }}">
+                                @include('alerts.feedback', ['field' => 'name'])
+                            </div>
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                <label>{{ __('Correo Electronico') }}</label>
+                                <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email address') }}" value="{{ old('email', auth()->user()->email) }}">
+                                @include('alerts.feedback', ['field' => 'email'])
+                            </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-fill btn-primary">{{ __('Guardar') }}</button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="title">{{ __('Contraseña') }}</h5>
+                </div>
+                <form method="post" action="{{ route('profile.password') }}" autocomplete="off">
+                    <div class="card-body">
+                        @csrf
+                        @method('put')
+
+                        @include('alerts.success', ['key' => 'password_status'])
+
+                        <div class="form-group{{ $errors->has('old_password') ? ' has-danger' : '' }}">
+                            <label>{{ __('Contraseña anterior') }}</label>
+                            <input type="password" name="old_password" class="form-control{{ $errors->has('old_password') ? ' is-invalid' : '' }}" placeholder="{{ __('Contraseña anterior') }}" value="" required>
+                            @include('alerts.feedback', ['field' => 'old_password'])
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            <label>{{ __('Nueva contraseña') }}</label>
+                            <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Nueva contraseña') }}" value="" required>
+                            @include('alerts.feedback', ['field' => 'password'])
+                        </div>
+                        <div class="form-group">
+                            <label>{{ __('Confirmar contraseña') }}</label>
+                            <input type="password" name="password_confirmation" class="form-control" placeholder="{{ __('Confirmar nueva contraseña') }}" value="" required>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-fill btn-primary">{{ __('Cambiar Contraseña') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card card-user">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="author">
+                            <div class="block block-one"></div>
+                            <div class="block block-two"></div>
+                            <div class="block block-three"></div>
+                            <div class="block block-four"></div>
+                            <a href="#">
+                                <img class="avatar" src="{{ asset('black') }}/img/jofre.png" alt="">
+                                <h5 class="title">{{ auth()->user()->name }}</h5>
+                            </a>
+                            <p class="description">
+                                {{ __('Administrador/a') }}
+                            </p>
+                        </div>
+                    </p>
+                    <div class="card-description">
+                        {{ __('40 años, 3 hijas, Divorciado, Ingeniero en software. Actualmente se desempeña como auxiliar administratvo en la entidad de la Registraduria Especial de Inirida') }}
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="button-container">
+                        <button class="btn btn-icon btn-round btn-facebook">
+                            <i class="fab fa-facebook"></i>
+                        </button>
+                        <button class="btn btn-icon btn-round btn-twitter">
+                            <i class="fab fa-twitter"></i>
+                        </button>
+                        <button class="btn btn-icon btn-round btn-google">
+                            <i class="fab fa-google-plus"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
