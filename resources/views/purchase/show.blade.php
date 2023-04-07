5df9f202b0858f617617b8779337d9e119199797
Jofre <jofre1706@gmail.com>
2023-04-07 17:07:55 -0500

    PURCHASE.SHOW

---
+++ resources/views/purchase/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app', ['page' => __('Compras'), 'pageSlug' => 'Show'])
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Detalle Compra</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body"></div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+@endsection
